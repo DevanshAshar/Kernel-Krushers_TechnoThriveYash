0f26a709993514742dc407b6f0fb6b75a9e4afaf
Dhruvin3103 <dcchawda2003@gmail.com>
2023-09-13 20:19:17 +0530

    hi

---
+++ backend/model/test_pickle.py
@@ -0,0 +1,11 @@
+import pickle
+from sentiment_analysis import predict_sentiment
+
+# Load the model and tokenizer from the pickle file
+with open('backend/model/sentiment_analysis_model.pkl', 'rb') as f:
+    model, tokenizer = pickle.load(f)
+
+# Predict the sentiment of a text
+text = 'i am feeling low.'
+response = predict_sentiment(text)
+print(type(response))
