c8677c07534d93b0d67b977faffe95a3f12f1e03
Devansh Ashar <ashar.devansh@gmail.com>
2023-09-23 08:30:18 +0530

    small change

--- client/src/Components/Room.jsx
+++ client/src/Components/Room.jsx
@@ -5,8 +5,8 @@ import Layout from '../Layout/Layout'
 const Room = () => {
     const {id}=useParams()
     const meeting=async(element)=>{
-        const appID=214113188
-        const serverSecret="7604664862c7dd8f30e5e26ac303833b"
+        const appID=Number(process.env.REACT_APP_appID)
+        const serverSecret=process.env.REACT_APP_serverSecret
         const kitToken=ZegoUIKitPrebuilt.generateKitTokenForTest(appID,serverSecret,id,Date.now().toString(),"User")
         const zc=ZegoUIKitPrebuilt.create(kitToken)
         zc.joinRoom({
